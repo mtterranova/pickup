a91c24e2b66ead81ff531ffc80772242fd980af2
Dailen Spencer <dailenspencer@gmail.com>
2016-07-20 17:32:56 -0700

    [Feature] Add Profile Avatar

--- client/public/components/Navigation/Profile/ProfileAvatar.js
+++ client/public/components/Navigation/Profile/ProfileAvatar.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
-import Avatar from 'material-ui/Avatar';
+import Avatar from 'material-ui/Avatar/Avatar';
+import ProfileMenu from './ProfileMenu';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import ListItem from 'material-ui/List/ListItem';
 import List from 'material-ui/List/List';
+import $ from 'jquery';
 
-class ProfileAvatar extends Component {
+export default class ProfileAvatar extends Component {
   constructor(props) {
     super(props)
 }
+
+
+  handleAvatarClick(){
+    console.log('handle avatar click');
+    $("#ProfileMenu").removeClass("SlideOut");
+    $("#ProfileMenu").addClass("SlideIn");
+  }
 
   render() {
     return(
@@ -16,12 +25,12 @@ class ProfileAvatar extends Component {
          <Avatar
            style={{'width':'45px','height':'45px'}}
            src="https://avatars0.githubusercontent.com/u/13908543?v=3&s=400"
+           onClick={this.handleAvatarClick}
           />
         </MuiThemeProvider>
-
-      </div>
+        <ProfileMenu/>
+      </div> 
     )
   }
 }
 
-export default ProfileAvatar;
