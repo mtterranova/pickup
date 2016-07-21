e17ea77a0bd0f4b44bda9e3c84d690900b272330
Matthew Terranova <MattTerranova92@gmail.com>
2016-07-21 13:20:55 -0700

    [Feature] add game popup form

--- client/public/components/Navigation/NavBar.js
+++ client/public/components/Navigation/NavBar.js
@@ -1,32 +1,50 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import ProfileAvatar from './Profile/ProfileAvatar'
 import HamburgerMenu from './Drawer/HamburgerMenu'
 import AddButton from './addButton'
 import SearchBar from './SearchBar'
+import Add from '../../containers/add'
 
 export default class NavBar extends Component {
-  constructor(props) {
-    super(props)
-}
-  nav(){
-    return(
+	constructor(props) {
+		super(props)
+		this.state = {
+			addForm: ''
+		}
+	}
 
-      <div id="Navigation">
-       <ProfileAvatar/>
-       <HamburgerMenu/>
-       <h1 id="title" className = "text-center">PickUpz</h1>
-       <AddButton/>
-       <SearchBar/>
-      </div>
+	handleAddFormClick() {
+		this.setState({ addForm: 'FadeIn' })
+	}
 
-      )
-  }
+  	nav(){
+		return (
 
-  render() {
-    return(
-    <div>
-        {this.nav()}
-    </div>
-    )
-  }
+			<div>
+				<div id="Navigation">
+			   		<ProfileAvatar />
+			   		<HamburgerMenu />
+			   		<h1 id="title" className = "text-center">PickUpz</h1>
+			   		<AddButton onClick = {this.handleAddFormClick.bind(this)} />
+			   		<SearchBar />
+			  	</div>
+
+			  	<div
+					id="addFormWrapper"
+					className={this.state.addForm}>
+
+					<Add id="addFormWrapper"/>
+
+				</div>
+			</div>
+		)
+	}
+
+  	render() {
+		return (
+			<div>
+				{this.nav()}
+			</div>
+		)
+  	}
 }
